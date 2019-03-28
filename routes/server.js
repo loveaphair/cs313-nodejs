fc8b7213959acb32e632f3ae1ec8d2b1302daaec
Kevin Phair <kevinphair@Kevins-MacBook-Pro.local>
2019-03-28 00:31:15 -0600

    updated the number of results returned and fixed som layout issues

--- routes/server.js
+++ routes/server.js
@@ -1,11 +1,12 @@
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('ae14a9ea6acf4d7d94b1e221351c1757');
 newsapi.v2.topHeadlines({
-		sources: 'bbc-news,the-verge',
+		sources: 'bbc-news,the-verge, associated-press, cnn, espn, fortune, fox-news, google-news, the-huffington-post, the-wall-street-journal, time, wired, the-washington-post',
 		// q: 'bitcoin',
 		// category: 'business',
 		language: 'en',
-		// country: 'us'
+		// country: 'us',
+		pageSize:100,
 	  }).then(response => {
 		module.exports.newsHeadlines = response;
 	  });
@@ -14,6 +15,7 @@ module.exports = {
 	searchNews: function (res, query, source){
 		newsapi.v2.topHeadlines({
 			sources: source,
+			pageSize:100,
 			q: query,
 			language: 'en',
 			sortBy: 'relevance'
